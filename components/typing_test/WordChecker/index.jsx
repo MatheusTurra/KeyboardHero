9e9efcabd6c23f321e832b6621f2b77d465481bd
MatheusTurra <matheusfr.318@gmail.com>
2022-03-18 00:12:22 -0300

    Lógica para limpar o input do usuário

--- components/typing_test/WordChecker/index.jsx
+++ components/typing_test/WordChecker/index.jsx
@@ -1,15 +1,38 @@
+import { useRef } from "react";
+
 import styles from "./styles.module.css"
 
+const text = "Esse é o primeiro teste para projeto Keyboard Hero";
+const splittedText = text.split();
+
 export default function WordChecker() {
+  const userInput = useRef();
+
+  function verifyWord(event) {
+    const keyPress = event.code;
+
+    if(keyPress === "Space") {
+      userInput.current.reset();
+    }
+  }
+
   return(
     <>
       <section className={styles.container}>
         <h1 className={styles.title}>Keyboard Hero</h1>
-        <textarea 
+        <textarea
+          readOnly
+          defaultValue={text}
           className={styles.testText}
         />
         <div className={styles.userInputContainer}>
-          <input type="text" className={styles.userInput}/>
+          <form ref={userInput}>
+            <input
+              type="text"
+              onKeyUp={event => verifyWord(event)}
+              className={styles.userInput}
+            />
+          </form>
         </div>
       </section>
     </>
