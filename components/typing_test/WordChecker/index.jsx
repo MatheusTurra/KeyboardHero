7af73bcbda92ac8f393594d533fd229d5d0e34ef
MatheusTurra <matheusfr.318@gmail.com>
2022-03-17 22:44:02 -0300

    input para o usuário fazer o teste

--- components/typing_test/WordChecker/index.jsx
+++ components/typing_test/WordChecker/index.jsx
@@ -1,13 +1,16 @@
 import styles from "./styles.module.css"
 
-export default function Test() {
+export default function WordChecker() {
   return(
     <>
       <section className={styles.container}>
         <h1 className={styles.title}>Keyboard Hero</h1>
         <textarea 
-          className={styles.testInput}
+          className={styles.testText}
         />
+        <div className={styles.userInputContainer}>
+          <input type="text" className={styles.userInput}/>
+        </div>
       </section>
     </>
   );
