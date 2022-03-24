21be96e88853e40b43a4492eafc737f18f84287d
MatheusTurra <matheusfr.318@gmail.com>
2022-03-24 14:53:56 -0300

    lógica básica do teste

--- components/typing_test/WordChecker/index.jsx
+++ components/typing_test/WordChecker/index.jsx
@@ -2,27 +2,37 @@ import { useRef } from "react";
 
 import styles from "./styles.module.css"
 
-let wordIndex = 0;
-let letterIndex = 0;
+let textIndex = 0;
+let textLetterIndex = 0;
 
 const text = "Esse é o primeiro teste para o projeto Keyboard Hero";
 const splittedText = text.split(" ");
 
 export default function WordChecker() {
-  
+  const userInputRef = useRef();
+
   function verifyLetters(event){
-    const textLetters = splittedText[0].split("");
+    const textLetters = splittedText[textIndex].split("");
     const inputData = event.nativeEvent.data;
     const userInputValue = event.target.value;
     const userInputIndex = userInputValue.length - 1;
 
-    if(textLetters[letterIndex] === userInputValue[userInputIndex]) {
-      letterIndex++;
+    if(
+      textLetterIndex < splittedText[textIndex].length -1 &&
+      textLetters[textLetterIndex] === userInputValue[userInputIndex]
+    ) {
+      textLetterIndex++;
     }
 
-    if(inputData === null && letterIndex >= userInputIndex) {
-      if(letterIndex > 0) {
-        letterIndex--;
+    if(inputData === " "){
+      textIndex++;
+      textLetterIndex = 0;
+      userInputRef.current.reset();
+    }
+    
+    if(inputData === null && textLetterIndex >= userInputIndex) {
+      if(textLetterIndex > 0) {
+        textLetterIndex--;
       }
     }
   }
@@ -36,7 +46,7 @@ export default function WordChecker() {
           className={styles.testText}
         />
         <div className={styles.userInputContainer}>
-          <form>
+          <form ref={userInputRef}>
             <input
               type="text"
               onChange={event => verifyLetters(event)}
