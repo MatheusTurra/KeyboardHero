fa25240aab56aea0f69b8588461ae9b10cbadeeb
MatheusTurra <matheusfr.318@gmail.com>
2022-03-25 11:34:59 -0300

    contador de palavras movido para estado

--- components/typing_test/WordChecker/index.jsx
+++ components/typing_test/WordChecker/index.jsx
@@ -1,10 +1,9 @@
 import RenderText from "../RenderText";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import styles from "./styles.module.css"
 
-let textIndex = 0;
 let textLetterIndex = 0;
 
 const text = "Esse é o primeiro teste para o projeto Keyboard Hero";
@@ -12,37 +11,46 @@ const splittedText = text.split(" ");
 
 export default function WordChecker() {
   const userInputRef = useRef();
+  const [textIndex, setTextIndex] = useState(0);
 
   function verifyLetters(event){
-    const textLetters = splittedText[textIndex].split("");
     const inputData = event.nativeEvent.data;
+    const textLetters = splittedText[textIndex].split("");
     const userInputValue = event.target.value;
     const userInputIndex = userInputValue.length - 1;
 
     if(
       textLetterIndex < splittedText[textIndex].length -1 &&
       textLetters[textLetterIndex] === userInputValue[userInputIndex]
-    ) {
+    ){
       textLetterIndex++;
     }
 
     if(inputData === " "){
-      textIndex++;
+      incrementTextIndex();
       textLetterIndex = 0;
       userInputRef.current.reset();
     }
-    
     if(inputData === null && textLetterIndex >= userInputIndex) {
       if(textLetterIndex > 0) {
         textLetterIndex--;
       }
     }
   }
+
+  function incrementTextIndex() {
+    setTextIndex(prevState => {
+      return prevState + 1;
+    });
+  } 
   return(
     <>
       <section className={styles.container}>
         <h1 className={styles.title}>Keyboard Hero</h1>
-        <RenderText text={text}/>
+        <RenderText 
+          text={text}
+          textIndex={textIndex}
+        />
         <div className={styles.userInputContainer}>
           <form ref={userInputRef}>
             <input
