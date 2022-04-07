68a79b6c3330817bd4b47793299964669b2ed2b4
MatheusTurra <matheusfr.318@gmail.com>
2022-04-07 19:13:06 -0300

    feat: Cálculo de palavras por minuto (wpm)

--- components/typing_test/Result/index.jsx
+++ components/typing_test/Result/index.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 
 export default function Result(props) {
+  const [wpm, setWpm] = useState(0);
   const [accuracy, setAccuracy] = useState(0);
 
-   useEffect(() => {
-     if(props.keyPresses > 0) {
-       const accuracyCalc = (props.correctKeyPresses / props.keyPresses) * 100;
+  useEffect(() => {
+    if(props.keyPresses > 0) {
+      const accuracyCalc = (props.correctKeyPresses / props.keyPresses) * 100;
 
-       setAccuracy(accuracyCalc.toFixed());
-     }
+      setAccuracy(accuracyCalc.toFixed());
+    }
   }, [props.correctKeyPresses]);
-    return(
+
+
+  useEffect(() => {
+    const wpmCalc = ((props.correctKeyPresses - props.incorrectKeyPresses) / 5) / (props.maxTime - props.timeLeft) * 60; 
+    setWpm(wpmCalc.toFixed());
+  }, [props.timeLeft]);
+
+  return(
     <>
       <div>
-        <span>: {accuracy}</span>
+        <p>{wpm} : {accuracy}</p>
       </div>
     </>
   );
