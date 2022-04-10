419e3f2b62b39ab6e7a18ce16e70861657fb1ab3
MatheusTurra <matheusfr.318@gmail.com>
2022-04-10 18:46:55 -0300

    refactor: switcher de tema renomeado

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import ThemeSwitcher from "../components/interface/ThemeSwitcher";
 import WordChecker from "../components/typing_test/WordChecker";
 
+import { useState } from "react";
+
 export default function Home() {
+  const [toggled, setToggled] = useState(true);
+
+  function handleToggleSwitch() {
+    setToggled(prevState => !prevState);
+  }
+
+  console.log(toggled)
   return (
     <>
+      <ThemeSwitcher toggled={toggled} onClick={handleToggleSwitch}/>
       <WordChecker />
     </>
   );
