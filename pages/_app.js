ff4100378066665dab0929d04bd72c3216e0dbd7
MatheusTurra <matheusfr.318@gmail.com>
2022-04-12 20:05:23 -0300

    feat: Tema persistente com cookies

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,9 @@
 import GlobalStyle from "../styles/GlobalStyle";
-import { ThemeProvider } from "styled-components";
-
-import light from "../styles/themes/light";
-import dark from "../styles/themes/dark";
-import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useState(dark);
-
-  function handleThemeChange() {
-    setTheme(theme.title === "light" ? dark : light);
-  }
   return(
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} changeTheme={handleThemeChange}/>
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,64 @@ import Header from "../components/layout/Header";
 import ThemeSwitcher from "../components/interface/ThemeSwitcher";
 import WordChecker from "../components/typing_test/WordChecker";
 
+import dark from "../styles/themes/dark";
+import light from "../styles/themes/light";
+import GlobalStyle from "../styles/GlobalStyle";
+
+import { parseCookies, setCookie } from "nookies";
+
+import { ThemeProvider } from "styled-components";
+
 import { useState } from "react";
 
-export default function Home({changeTheme}) {
+export default function Home(props) {
+  const [theme, setTheme] = useState(props.theme);
   const [isDarkModeOn, setIsDarkModeOn] = useState(true);
 
+  function setPersistedTheme(theme) {
+    const twoDaysInSeconds = 8600 * 2;
+
+    setTheme(theme);
+    setCookie(null,  "theme", JSON.stringify(theme), {
+      path: "/",
+      maxAge: twoDaysInSeconds
+    });
+  }
+
   function handleThemeChange() {
-    setIsDarkModeOn(prevState => !prevState);
-    changeTheme();
+    setIsDarkModeOn(prevState => {
+      const newState = !prevState;
+      
+      if(newState) setPersistedTheme(dark);
+      else setPersistedTheme(light)
+      return newState;
+    });
   }
 
   return (
     <>
-      <Header />
-      <ThemeSwitcher toggled={isDarkModeOn} onClick={handleThemeChange}/>
-      <WordChecker />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <ThemeSwitcher toggled={isDarkModeOn} onClick={handleThemeChange}/>
+        <WordChecker />
+      </ ThemeProvider >
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  let storedTheme = dark;
+  const userCookies = parseCookies(context);
+
+  if(userCookies.theme) {
+    storedTheme = JSON.parse(userCookies.theme);
+    console.log(storedTheme)
+  }
+
+  return {
+    props: {
+      theme: storedTheme
+    }
+  }
 }
