6ae8cacafb43a02dddec4d2e0432385c19e9d918
MatheusTurra <matheusfr.318@gmail.com>
2022-04-14 10:02:08 -0300

    Responsividade do cronômetro

--- components/typing_test/Timer/styles.js
+++ components/typing_test/Timer/styles.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const TimerContainer = styled.div`
   display: flex;
+  width: 8rem;
   justify-content: center;
   align-items: center;
   font-size: 2.5rem;
   font-weight: 600;
-  margin: 0 3rem;
   border-radius: 1rem;
   background-color: ${props => props.theme.colors.secondary};
 `;
