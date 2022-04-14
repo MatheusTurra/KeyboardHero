46d6422eeaa94226a0b3ed04658b65d99ab79b06
MatheusTurra <matheusfr.318@gmail.com>
2022-04-14 11:49:58 -0300

    feat: Animação do botão de reiniciar jogo

--- components/interface/ResetButton/index.jsx
+++ components/interface/ResetButton/index.jsx
@@ -1,10 +1,10 @@
 import { MdRefresh } from "react-icons/md";
 import { ButtonContainer } from "./styles";
 
-export default function ResetButton({isDarkModeOn}) {
+export default function ResetButton({isDarkModeOn, onClick}) {
   return(
     <>
-      <ButtonContainer isDarkModeOn={isDarkModeOn}>
+      <ButtonContainer isDarkModeOn={isDarkModeOn} onClick={onClick}>
         <MdRefresh />
       </ButtonContainer>
     </>
--- components/interface/ResetButton/styles.js
+++ components/interface/ResetButton/styles.js
@@ -1,18 +1,56 @@
 import styled from "styled-components";
 
-export const ButtonContainer = styled.div`
+export const ButtonContainer = styled.button`
   width: 65%;
   height: 4rem;
   cursor: pointer;
   font-size: 2.8rem;
   font-weight: 600;
+  position: relative;
   margin: 0 auto;
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 1rem;
+  border: 0;
   color: ${props => props.theme.colors.text};
   background-color: ${props => props.theme.colors.secondary};
+
+  & :after {
+    content: "";
+    display: block;
+    position: absolute;
+    border-radius: 4em;
+    border: 0;
+    left: 0;
+    top:0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: all 0.3s;
+    box-shadow: ${props => props.isDarkModeOn
+      ? "0 0 1rem 4rem #30c5d2"
+      : "0 0 1rem 4rem #f86ca7"};
+  }
+
+  & :active:after {
+    box-shadow: ${props => props.isDarkModeOn
+      ? "0 0 0 0 #30c5d2"
+      : "0 0 0 0 #f86ca7"};
+
+    position: absolute;
+    border: 0;
+    border-radius: 1rem;
+    left: 0;
+    top:0;
+    opacity: 1;
+    transition: 0s;
+  }
+
+  & :active {
+    top: 1px;
+  } 
+
 
   & :hover {
     box-shadow: 0.7rem 0.5rem 1rem #000;
