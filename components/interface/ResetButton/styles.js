1f0c20d956180d446b1c557fc6b95f8433f02b6a
MatheusTurra <matheusfr.318@gmail.com>
2022-04-17 17:30:09 -0300

    Animação do botão de resetar consertada no mobile

--- components/interface/ResetButton/styles.js
+++ components/interface/ResetButton/styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-  width: 65%;
+  /* width: 65%; */
+  width: 30%;
   height: 5rem;
   cursor: pointer;
   font-size: 2.8rem;
@@ -14,20 +15,20 @@ export const ButtonContainer = styled.button`
   border-radius: 1rem;
   border: 0;
   color: ${props => props.theme.colors.text};
-  background-color: ${props => props.theme.colors.background};
+  background-color: ${props => props.theme.colors.secondary};
 
   & :after {
     content: "";
     display: block;
     position: absolute;
-    border-radius: 4em;
+    border-radius: 1rem;
     border: 0;
     left: 0;
     top:0;
     width: 100%;
     height: 100%;
     opacity: 0;
-    transition: all 0.3s;
+    transition: all 1s;
     box-shadow: ${props => props.isDarkModeOn
       ? "0 0 1rem 3rem #30c5d2"
       : "0 0 1rem 3rem #f86ca7"};
@@ -49,15 +50,28 @@ export const ButtonContainer = styled.button`
 
   & :active {
     top: 1px;
-  } 
-
-
-  & :hover {
+    border-radius: 1rem;
     box-shadow: 0.7rem 0.5rem 1rem #000;
-
+    transition: 1s;
+    
     background-image: ${props => props.isDarkModeOn
       ? "linear-gradient(to right, #471069, #30c5d2)"
       : "linear-gradient(to right, #f86ca7, #f4d444)"
     };
+
+  } 
+
+  @media(min-width: 768px) {
+    width: 65%;
+
+    & :hover {
+      box-shadow: 0.7rem 0.5rem 1rem #000;
+  
+      background-image: ${props => props.isDarkModeOn
+        ? "linear-gradient(to right, #471069, #30c5d2)"
+        : "linear-gradient(to right, #f86ca7, #f4d444)"
+      };
+    }
   }
+
 `;
