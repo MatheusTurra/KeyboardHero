501c086e019a6b18606492dc7fbd95bdb2b7801f
MatheusTurra <matheusfr.318@gmail.com>
2022-04-18 20:35:14 -0300

    Componente footer adicionado

---
+++ components/layout/Footer/index.jsx
@@ -0,0 +1,25 @@
+import { Container, FooterTitle, SocialMediaLinks } from "./styles";
+
+export default function Footer(){
+  return(
+    <Container>
+      <FooterTitle>
+        <h1>Feito por Matheus Farisco turra</h1>
+      </FooterTitle>
+      <SocialMediaLinks>
+        <a 
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/matheus-farisco-turra-6837a6214/">
+          Linkedin
+        </a>
+        <a 
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/MatheusTurra">
+          Github
+        </a>
+      </SocialMediaLinks>
+    </Container>
+  );
+}
---
+++ components/layout/Footer/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 10rem;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const FooterTitle = styled.div`
+  font-size: 0.9rem;
+  color: ${props => props.theme.colors.text};
+`;
+
+export const SocialMediaLinks = styled.div`
+  font-size: 1.4rem;
+  margin-top: 1rem;
+
+  & a{
+    padding: 0.7rem;
+    margin: 0 1.2rem;
+    cursor: pointer;
+    text-decoration: none;
+    color: ${props => props.theme.colors.text};
+  }
+
+  & a:hover {
+    border-radius: 1rem;
+    background-color: ${props => props.theme.colors.link};
+    box-shadow: 0 0 2rem 0.2rem ${props => props.theme.colors.link};
+  }
+`;
