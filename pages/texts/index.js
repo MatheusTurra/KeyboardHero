0a6730b6d97b3088b9d15b11949f851fcbbef64b
MatheusTurra <matheusfr.318@gmail.com>
2022-04-23 10:54:59 -0300

    Página do modo texto

---
+++ pages/texts/index.js
@@ -0,0 +1,74 @@
+import Seo from "../../components/Seo";
+import Header from "../../components/layout/Header";
+import Footer from "../../components/layout/Footer";
+import TextChecker from "../../components/typing_test/TextChecker";
+
+import dark from "../../styles/themes/dark";
+import light from "../../styles/themes/light";
+import GlobalStyle from "../../styles/GlobalStyle";
+
+import { parseCookies, setCookie } from "nookies";
+import { ThemeProvider } from "styled-components";
+
+import { useState } from "react";
+
+/**
+ * TODO: CRIAR UM CONTEXTO PARA ARMAZENAR O ESTADO DO TEMA
+ */
+
+export default function Home(props) {
+  const [theme, setTheme] = useState(props.theme);
+  const [isDarkModeOn, setIsDarkModeOn] = useState(getThemeStatus);
+
+  function getThemeStatus() {
+    if(props.theme.title === "dark") return true;
+    else return false;
+  }
+
+  function setPersistedTheme(theme) {
+    const twoDaysInSeconds = 8600 * 2;
+
+    setTheme(theme);
+    setCookie(null,  "theme", JSON.stringify(theme), {
+      path: "/",
+      maxAge: twoDaysInSeconds
+    });
+  }
+
+  function handleThemeChange() {
+    setIsDarkModeOn(prevState => {
+      const newState = !prevState;
+      
+      if(newState) setPersistedTheme(dark);
+      else setPersistedTheme(light)
+      return newState;
+    });
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Seo />
+        <GlobalStyle />
+        <Header isDarkModeOn={isDarkModeOn} onClick={handleThemeChange}/>
+        <TextChecker isDarkModeOn={isDarkModeOn} />
+        <Footer />
+      </ ThemeProvider >
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  let storedTheme = light;
+  const userCookies = parseCookies(context);
+
+  if(userCookies.theme) {
+    storedTheme = JSON.parse(userCookies.theme);
+  }
+
+  return {
+    props: {
+      theme: storedTheme
+    }
+  }
+}
