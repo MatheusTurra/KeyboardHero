ef1ee6e319c4b29a64762aef8e3993f225efaa98
MatheusTurra <matheusfr.318@gmail.com>
2022-05-04 22:46:18 -0300

    Footer comentado temporariamente

--- pages/texts/index.js
+++ pages/texts/index.js
@@ -56,7 +56,7 @@ export default function Home(props) {
         <GameProvider>
           <TextChecker isDarkModeOn={isDarkModeOn} />
         </GameProvider>
-        <Footer />
+        {/* <Footer /> */}
       </ ThemeProvider >
     </>
   );
