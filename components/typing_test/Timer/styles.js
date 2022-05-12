8ba0efeb6086939571e78c1cb8a2d674c1c1ff9f
MatheusTurra <matheusfr.318@gmail.com>
2022-05-12 00:30:59 -0300

    feat: Temporizador escondido

--- components/typing_test/Timer/styles.js
+++ components/typing_test/Timer/styles.js
@@ -4,7 +4,7 @@ export const TimerContainer = styled.div`
   width: 8rem;
   height: 5rem;
   margin: 0 auto;
-  display: ${props => props.showTimer ? "flex" : "none"};
+  display: flex;
   flex-grow: 1;
   justify-content: center;
   align-items: center;
@@ -12,4 +12,8 @@ export const TimerContainer = styled.div`
   font-weight: 600;
   border-radius: 1rem;
   background-color: ${props => props.theme.colors.secondary};
+
+  & span {
+    display: ${props => props.showTimer === false && "none"};
+  }
 `;
